ef3f6477fb92c718d6d253c4bd799da13d2a9b9e
sparkletech <wangyunrui001@sina.com>
2016-05-17 13:40:54 +0800

    初始化项目

---
+++ TableviewTest/TableviewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableviewTest
+//
+//  Created by wangyunrui on 16/5/17.
+//  Copyright © 2016年 sparkletech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableviewTest/TableviewTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TableviewTest
+//
+//  Created by wangyunrui on 16/5/17.
+//  Copyright © 2016年 sparkletech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"aaa");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
